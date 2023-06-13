e5f37170b929d755ef8d8f3f6f11e1253b2b59cb
massamba08 <samndodia@gmail.com>
2023-06-14 00:18:41 +0200

    installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './components/cinema/cinema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent
   ],
   imports: [
     BrowserModule
